83c6530e3d9427966dc74d4662f4b2b16519fe1d
Saviola11 <119750239+Saviola11@users.noreply.github.com>
2024-09-13 13:53:26 +0200

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+const burgerBtn = document.querySelector('.hamburger')
+const navMobile = document.querySelector('.nav')
+const msgStatus = document.querySelector('.msg-status')
+
+console.log(document.location.search);
+
+if (document.location.search === '?mail_status=sent') {
+	msgStatus.classList.add('success')
+	msgStatus.textContent = 'Wiadomość wysłana!'
+
+	setTimeout(() => {
+		msgStatus.classList.remove('success')
+	}, 3000);
+}
+
+if (document.location.search === '?mail_status=error') {
+	msgStatus.classList.add('error')
+	msgStatus.textContent = 'Wystąpił błąd.'
+
+	setTimeout(() => {
+		msgStatus.classList.remove('error')
+	}, 3000);
+}
+
+const burgerMenu = () => {
+    console.log('hej');
+    navMobile.classList.toggle('nav-mobile-active')
+}
+
+burgerBtn.addEventListener('click', burgerMenu)
